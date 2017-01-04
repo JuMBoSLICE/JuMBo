a43fa340ff9d4059e066dbd62f13e4240f0853fd
listentomm <maxmarcususc@gmail.com>
2017-01-04 11:28:16 -0800

    database models set up

--- src/server/database.js
+++ src/server/database.js
@@ -11,41 +11,87 @@ const Users = sequelize.define('users', {
   password: {
     type: Sequelize.STRING,
     field: 'password',
+  },
+  name: {
+    type: Sequelize.STRING,
+    field: 'first_name',
   }
 }, {
   freezeTableName: true
 });
 
-// add new user method (on sign-up)
-// Users.sync().then(function () { //forces drop and re-add of table
-//   Users.create().then(function (user) { });
-// });
-
 
 // Projects table
 const Projects = sequelize.define('projects', {
-  project: {
+  title: {
     type: Sequelize.STRING,
-    field: 'project_name',
-  },
-  users: {
-    type: Sequelize.INTEGER, // ???
-    field: 'project_users',
+    field: 'project_title',
   },
   summary: {
     type: Sequelize.STRING,
     field: 'summary',
   },
-  tasks: {
-    type: Sequelize.INTEGER, // ???
-    field: 'tasks',
-  }
 }, {
   freezeTableName: true
 });
 
 
 // Tasks table
-// const Tasks = sequelize.define('tasks', {
-  
-// })
+const Tasks = sequelize.define('tasks', {
+  taskName: {
+    type: Sequelize.STRING,
+    field: 'task_name',
+  },
+  status: {
+    type: Sequelize.STRING,
+    field: 'status',
+  },
+  owner: {
+    type: Sequelize.INTEGER,
+    field: 'assigned_to',
+  },
+  content: {
+    type: Sequelize.STRING,
+    field: 'task_content',
+  },
+}, {
+  freezeTableName: true
+});
+
+// add new user method (on sign-up)
+Users.sync().then(function () { //forces drop and re-add of table
+  // Users.create({
+  //   username: 'batman',
+  //   password: 'rachel',
+  //   name: 'morgan'
+  // }).then(function (user) {
+  //   user.setProjects(Projects, {title: 'save rachel', summary: 'kill joker'})
+  // });
+});
+
+Projects.sync().then(function () { //forces drop and re-add of table
+  // Projects.create().then(function (project) {
+  // });
+});
+
+Tasks.sync({ force: true }).then(function () { //forces drop and re-add of table
+  Tasks.create({
+    taskName: 'new suit',
+    status: 'in progress',
+    owner: 1,
+    content: 'blah blah'
+  }).then(function (task) { });
+});
+
+Users.belongsToMany(Projects, { through: 'ProjectUsers' });
+Projects.belongsToMany(Users, { through: 'ProjectUsers' });
+
+Users.create({
+  username: 'batman',
+  password: 'rachel',
+  name: 'morgan'
+}).then(function (user) {
+  user.setProjects(Projects, { title: 'save rachel', summary: 'kill joker' })
+});
+
+Tasks.belongsTo(Projects);
