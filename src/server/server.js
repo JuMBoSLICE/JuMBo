3079ada46c254c4df2b240f4a23d71f8d0cd5a17
listentomm <maxmarcususc@gmail.com>
2017-01-03 21:20:34 -0800

    not sure

---
+++ src/server/server.js
@@ -0,0 +1,7 @@
+var pg = require('pg');
+var Sequelize = require('sequelize');
+var express = require('express');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
+var app = express();
