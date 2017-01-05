74c87f20b33eb3808085363950193e13aaed9a46
Michael Richards <mrichards@Michaels-MacBook-Pro.local>
2017-01-05 11:30:29 -0800

    updated login and signup with new styling

--- src/client/app/components/login.js
+++ src/client/app/components/login.js
@@ -8,7 +8,6 @@ class Login extends Component {
   render() {
     return (
       <div id='auth'>
-<<<<<<< HEAD
         <img src="http://i.imgur.com/dLQMwZp.png" className="logo" />
         <h2>CodeLaborate</h2>
         <h4>Log in</h4>
@@ -18,14 +17,6 @@ class Login extends Component {
           <button onClick={ () => {this.props.userVerify()}} >Log in</button>
         </form>
         <p>New User? <a onClick={this.props.newRegistration}>Sign up here.</a></p>
-=======
-        <h2>Welcome to codeLaborate</h2>
-        <h4>Log in!</h4>
-          <input className='username' type='text' placeholder='Username' value={this.props.username} onChange={ (e) => {this.props.usernameChange(e)}}></input>
-          <input className='password' type='password' placeholder='Password' value={this.props.password} onChange={ (e) => {this.props.passwordChange(e)}}></input>
-          <button onClick={ () => {this.props.userVerify()}} >Login</button>
-        <p><a onClick={this.props.newRegistration}>New User?</a></p>
->>>>>>> 85de30e56ae9515ab6302633c62bbf445d0bd7ec
       </div>
     )
   }
--- src/client/app/components/signup.js
+++ src/client/app/components/signup.js
@@ -6,7 +6,6 @@ class Signup extends Component {
   render() {
     console.log(this.props)
     return (
-<<<<<<< HEAD
         <div id='auth'>
         <img src="http://i.imgur.com/dLQMwZp.png" className="logo" />
         <h2>CodeLaborate</h2>
@@ -18,17 +17,6 @@ class Signup extends Component {
 
           <p><a onClick={this.props.newRegistration}>Actually, I have an account!</a></p>
         </div>
-=======
-      <div id='auth'>
-      <h4>Sign up!</h4>
-        <input className='name' type='text' placeholder='Your Name' value={this.props.name} onChange={(e) => {this.props.nameChange(e)}}></input>
-        <input className='username' type='text' placeholder='Username' value={this.props.username} onChange={ (e) => {this.props.usernameChange(e)}}></input>
-        <input className='password' type='password' placeholder='Password' value={this.props.password} onChange={ (e) => {this.props.passwordChange(e)}}></input>
-        <button onClick={ () => {this.props.signUpPost()}}>Register</button>
-
-        <p><a onClick={this.props.newRegistration}>Actually, I have an account!</a></p>
-      </div>
->>>>>>> 85de30e56ae9515ab6302633c62bbf445d0bd7ec
     )
   }
 }
