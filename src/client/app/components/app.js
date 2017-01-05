bc215dd1b676d35a55ec764ce2ca30ba44c7e2e6
admin <admin@admins-iMac.local>
2017-01-04 22:24:42 -0800

    1.4.16.10:25 registration page working

--- src/client/app/components/app.js
+++ src/client/app/components/app.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
 import Login from './login.js';
-import { render } from 'react-dom';
 import Signup from './signup.js';
 import styles from './../../style.css';
 import Dashboard from './dashboard.js';
+import axios from 'axios';
 
 
 class App extends Component {
 
- constructor() {
-    super();
+ constructor(props) {
+    super(props);
     this.state = {
-      page: 0
+      page: 0,
+      name: '',
+      username: '', 
+      password: '', 
     }
-    this.newRegistration = this.newRegistration.bind(this)
+    this.newRegistration = this.newRegistration.bind(this);
+    this.signUpPost = this.signUpPost.bind(this);
+    this.usernameChange = this.usernameChange.bind(this);
+    this.nameChange = this.nameChange.bind(this);
+    this.passwordChange = this.passwordChange.bind(this);
+    this.userVerify = this.userVerify.bind(this);
   }
 
   newRegistration() {
@@ -24,7 +32,57 @@ class App extends Component {
     }
   }
 
+  signUpPost() {
+    console.log('inside signuppost')
+      axios.post('/signup', {
+        username: this.state.username,
+        name: this.state.name,
+        password: this.state.password,
+      }).then(function(response){
+          console.log(response);
+      }).catch(function(error) {
+          console.log(error);
+      }) 
+    this.setState({page: 0});
+  }
 
+
+    userVerify() {
+    console.log('inside user verify')
+      axios.post('/login', {
+        username: this.state.username,
+        password: this.state.password
+      }).then(function(response){
+          console.log(response);
+      }).catch(function(error) {
+          console.log(error);
+      }) 
+    this.setState({page: 0});
+  }
+
+  usernameChange(e) {
+      const state = {};
+      state.username = e.target.value;
+      this.setState(state);
+      console.log('this.state:', this.state);
+  }
+
+    nameChange(e) {
+    console.log('in formchange')
+      const state = {};
+      state.name = e.target.value;
+      this.setState(state);
+      console.log('this.state:', this.state);
+   
+  }
+
+    passwordChange(e) {
+      const state = {};
+      state.password= e.target.value;
+      this.setState(state);
+      console.log('this.state:', this.state);
+  
+  }
 
 render() {
 if (this.state.page === 0) {
@@ -32,6 +90,9 @@ return(
   <Login 
     newRegistration = {this.newRegistration}
     page={this.state.page}
+    userVerify = {this.userVerify}
+    usernameChange = {this.usernameChange}
+    passwordChange = {this.passwordChange}
     />
     )
 };
@@ -40,7 +101,13 @@ if (this.state.page === 1) {
   return( 
     <Signup 
       newRegistration = {this.newRegistration}
-
+      usernameChange = {this.usernameChange}
+      nameChange = {this.nameChange}
+      passwordChange = {this.passwordChange}
+      username = {this.state.username}
+      password = {this.state.password}
+      name = {this.state.name}
+      signUpPost = {this.signUpPost}
     />
   )
 }    
--- src/client/app/components/signup.js
+++ src/client/app/components/signup.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import { render } from 'react-dom';
+
 
 class Signup extends Component {
   render() {
+    console.log(this.props)
     return (
       <div id='auth'>
       <h4>Sign up!</h4>
-        <form method='POST'>
-          <input class='username' type='text' placeholder='Username'></input>
-          <input class='password' type='password' placeholder='Password'></input>
-          <button>Register</button>
-        </form>
+          <input className='name' type='text' placeholder='Your Name' value={this.props.name} onChange={(e) => {this.props.nameChange(e)}}></input>
+          <input className='username' type='text' placeholder='Username' value={this.props.username} onChange={ (e) => {this.props.usernameChange(e)}}></input>
+          <input className='password' type='password' placeholder='Password' value={this.props.password} onChange={ (e) => {this.props.passwordChange(e)}}></input>
+          <button onClick={ () => {this.props.signUpPost()}}>Register</button>
+
           <p><a onClick={this.props.newRegistration}>Actually, I have an account!</a></p>
       </div>
     )
