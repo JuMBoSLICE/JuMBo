e6e232261a8db4c12d87605a93e0025fdb064feb
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-01-06 21:33:23 -0800

    merging

--- src/server/database.js
+++ src/server/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://zzblcmad:7pM3odT6TkIDgJkOh4bhouvlGIWXdXxg@elmer.db.elephantsql.com:5432/zzblcmad');
+const sequelize = new Sequelize('postgres://gmopkzby:fzf6M75_2Pv39uBpHXDgbdDY8-iclyK1@elmer.db.elephantsql.com:5432/gmopkzby');
 
 sequelize.authenticate().then(function() {
   console.log('Connected!');
