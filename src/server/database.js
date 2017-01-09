a9ded3b2ab8a2eb34ef3746da59d9d5eeede5533
Michael Bieronski <mike@Michaels-MacBook-Pro.local>
2017-01-09 10:31:48 -0800

    added project components

--- src/server/database.js
+++ src/server/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-
+const sequelize = new Sequelize('postgres://cwnrvpli:yIg8-4inFfwDwP5mceLaTHWn8GBKMf5K@elmer.db.elephantsql.com:5432/cwnrvpli');
 
 
 sequelize.authenticate().then(function() {
