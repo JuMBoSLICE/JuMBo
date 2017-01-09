ccc9041fe21dc0eeaaa28b7e339de675a0ecddd5
Brandon Yuan <Brandon@Brandons-MacBook-Pro.local>
2017-01-09 10:47:59 -0800

    attempt at addTask

--- src/client/app/components/addTask.js
+++ src/client/app/components/addTask.js
@@ -11,20 +11,41 @@ class AddTask extends Component {
     }
   }
 
-  // titleChange(e) {
-  //   const state = {};
-  //   state.title = e.target.value;
-  //   this.setState(state);
-  //   // console.log(this.state);
-  // }
+  titleChange(e) {
+    const state = {};
+    state.title = e.target.value;
+    this.setState(state);
+    // console.log(this.state);
+  }
+
+  priorityChange(e) {
+    const state = {};
+    state.priority = e.target.value;
+    this.setState(state);
+    // console.log(this.state);
+  }
+
+  assignChange(e) {
+    const state = {};
+    state.assigned = e.target.value;
+    this.setState(state);
+    // console.log(this.state);
+  }
+
+  statusChange(e) {
+    const state = {};
+    state.status = e.target.value;
+    this.setState(state);
+    // console.log(this.state);
+  }
 
   render() {
     return (
       <li>
-        <h2> {this.state.title}</h2>
-        <p> Priority: {this.state.priority}</p>
-        <p> Priority: {this.state.assigned}</p>
-        <p> Priority: {this.state.status}</p>
+        <label>Task Title</label><input type="text" id="task-name" name="task-name"  onChange={ (e) => {this.titleChange(e)}} />
+        <label>Priority</label><input type="text" id="priority" name="priority"  onChange={ (e) => {this.priorityChange(e)}} />
+        <label>Assigned to:</label><input type="text" id="assigned" name="assigned"  onChange={ (e) => {this.assignChange(e)}} />
+        <label>Status:</label><input type="text" id="status" name="status"  onChange={ (e) => {this.statusChange(e)}} />
         <button className="erase">Erase Task</button>
         <button className="update">Update Task</button>
       </li>
